804913cb18d0b6e9f3ec7d572aa95575b8f87469
AndyCotruta <acotruta18@gmail.com>
2023-04-25 21:24:02 +0200

    added proper icon and title

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import LeftNav from "./components/LeftNav";
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [showMobileNav, setShowMobileNav] = useState(false);
 
   return (
     <BrowserRouter>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Weather App</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Nested component" />
+      </Helmet>
       <div className="d-flex">
         <LeftNav
           showMobileNav={showMobileNav}
